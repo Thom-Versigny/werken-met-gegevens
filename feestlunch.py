7e921ac9de98c0434050733975b1a498577de75e
Thom Versigny <thomversigny@gmail.com>
2021-09-08 13:17:54 +0200

    code added

---
+++ feestlunch.py
@@ -0,0 +1,4 @@
+print("17 x 0.39 + 2 x 2.78 - 3 x 0.50")
+print("=")
+print(17 * 0.39 + 2 * 2.78 - 3 * 0.50)
+input("Press enter to exit ;)")
---
+++ speelhaldag.py
@@ -0,0 +1,4 @@
+print("3 x 7.45 + 0.37 x 27")
+print("=")
+print(3 * 7.45 + 0.37 * 27)
+input("Press enter to exit ;)")
