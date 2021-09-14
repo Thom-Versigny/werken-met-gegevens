87f444c0f5bbcd8087cb4fa3688a2d67d20b0189
Thom Versigny <thomversigny@gmail.com>
2021-09-14 09:38:34 +0200

    code edited

--- feestlunch.py
+++ feestlunch.py
@@ -1,8 +1,3 @@
 A = 17 * 0.39 + 2 * 2.78 - 3 * 0.50
-B = 10.69
-print("17 x 0.39 + 2 x 2.78 - 3 x 0.50")
-print("=")
-print(A)
-if A == B:
-  print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 10.69 euro")
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar",A,)
 input("Press enter to exit ;)")
--- speelhaldag.py
+++ speelhaldag.py
@@ -1,8 +1,3 @@
 A = 3 * 7.45 + 0.37 * 27
-B = 32.34
-print("3 x 7.45 + 0.37 x 27")
-print("=")
-print(A)
-if A == B:
-	print("De feestlunch kost je bij de bakker 32.34 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
+print("De feestlunch kost je bij de bakker",A,"voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
 input("Press enter to exit ;)")
